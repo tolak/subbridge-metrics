15d423b24a9825e3d7e5d84ddbdbf138ec22d7c2
Wenfeng Wang <kalot.wang@gmail.com>
2022-04-17 18:32:05 +0800

    rename & fix

--- index.js
+++ index.js
@@ -3,96 +3,35 @@ require('dotenv').config();
 const express = require('express');
 const server = express();
 const register = require('prom-client').register;
-
-const reserve_balance = require('reserve-balance');
+const Gauge = require('prom-client').Gauge;
+const reserve_balances = require('./reserve-balances.js');
 const utils = require('./utils.js');
 const tokens = require('./tokens.json');
 
 // Enable collection of default metrics
-
 require('prom-client').collectDefaultMetrics({
 	gcDurationBuckets: [0.001, 0.01, 0.1, 1, 2, 5], // These are the default buckets.
 });
 
 // Create custom metrics
-const Histogram = require('prom-client').Histogram;
-const h = new Histogram({
-	name: 'test_histogram',
-	help: 'Example of a histogram',
-	labelNames: ['code'],
-});
-
-const Counter = require('prom-client').Counter;
-const c = new Counter({
-	name: 'test_counter',
-	help: 'Example of a counter',
-	labelNames: ['code'],
-});
-
-new Counter({
-	name: 'scrape_counter',
-	help: 'Number of scrapes (example of a counter with a collect fn)',
-	collect() {
-		// collect is invoked each time `register.metrics()` is called.
-		this.inc();
-	},
-});
-
-const Gauge = require('prom-client').Gauge;
-const g = new Gauge({
-	name: 'test_gauge',
-	help: 'Example of a gauge',
-	labelNames: ['method', 'code'],
-});
-
-const ReserveBalance = new Gauge({
-	name: 'reserve_balance',
-	help: 'Blance of reserve account on specific chain',
-	labelNames: ['token', 'chain'],
-});
+// const Histogram = require('prom-client').Histogram;
+// const h = new Histogram({
+// 	name: 'test_histogram',
+// 	help: 'Example of a histogram',
+// 	labelNames: ['code'],
+// });
 
 
 // Set metric values to some random values for demonstration
 
-setTimeout(() => {
-	h.labels('200').observe(Math.random());
-	h.labels('300').observe(Math.random());
-}, 10);
-
-setInterval(() => {
-	c.inc({ code: 200 });
-}, 5000);
-
-setInterval(() => {
-	c.inc({ code: 400 });
-}, 2000);
-
-setInterval(() => {
-	c.inc();
-}, 2000);
-
-setInterval(() => {
-	g.set({ method: 'get', code: 200 }, Math.random());
-	g.set(Math.random());
-	g.labels('post', '300').inc();
-}, 100);
+// setTimeout(() => {
+// 	h.labels('200').observe(Math.random());
+// 	h.labels('300').observe(Math.random());
+// }, 10);
 
 setInterval(async () => {
-    await reserve_balance.update_balance_of(tokens);
+    await reserve_balances.update_balance_of(tokens);
 }, 10000);  // 10s
-
-
-const t = [];
-setInterval(() => {
-	for (let i = 0; i < 100; i++) {
-		t.push(new Date());
-	}
-}, 10);
-setInterval(() => {
-	while (t.length > 0) {
-		t.pop();
-	}
-});
 
 // Setup server to Prometheus scrapes:
 
@@ -105,24 +44,7 @@ server.get('/metrics', async (req, res) => {
 	}
 });
 
-server.get('/metrics/counter', async (req, res) => {
-	try {
-		res.set('Content-Type', register.contentType);
-		res.end(await register.getSingleMetricAsString('test_counter'));
-	} catch (ex) {
-		res.status(500).end(ex);
-	}
-});
-
-server.get('/metrics/scrape', async (req, res) => {
-	try {
-		res.set('Content-Type', register.contentType);
-		res.end(await register.getSingleMetricAsString('scrape_counter'));
-	} catch (ex) {
-		res.status(500).end(ex);
-	}
-});
-
+/*
 server.get('/metrics/histogram', async (req, res) => {
 	try {
 		res.set('Content-Type', register.contentType);
@@ -131,15 +53,7 @@ server.get('/metrics/histogram', async (req, res) => {
 		res.status(500).end(ex);
 	}
 });
-
-server.get('/metrics/guage', async (req, res) => {
-	try {
-		res.set('Content-Type', register.contentType);
-		res.end(await register.getSingleMetricAsString('test_gauge'));
-	} catch (ex) {
-		res.status(500).end(ex);
-	}
-});
+*/
 
 server.get('/metrics/rb', async (req, res) => {
 	try {
@@ -150,7 +64,7 @@ server.get('/metrics/rb', async (req, res) => {
 	}
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 console.log(
 	`Server listening to ${port}, metrics exposed on /metrics endpoint`,
 );
--- reserve-balances.js
+++ reserve-balances.js
@@ -2,28 +2,45 @@
 const IERC20Json = require('./IERC20.json');
 const ethers = require('ethers');
 const { ApiPromise, WsProvider } = require("@polkadot/api");
+const Gauge = require('prom-client').Gauge;
+const utils = require('./utils.js');
+
+let khalaApi = 'undefined';
+let moonriverProvider = 'undefined';
+
+const ReserveBalance = new Gauge({
+	name: 'reserve_balance',
+	help: 'Blance of reserve account on specific chain',
+	labelNames: ['token', 'chain'],
+});
 
 async function update_balance_of(tokens) {
     Promise.all(tokens.map(async token => {
         let balance = 'undefined';
         if (token.chain_bype === 'sub') {
-            const provider = new WsProvider(token.endpoint);
-            const api = await ApiPromise.create({provider});
+            if (khalaApi === 'undefined') {
+                console.log(`Establish connection with substrate node`);
+                const provider = new WsProvider(token.endpoint);
+                khalaApi = await ApiPromise.create({provider});
+            }
             if (token.is_native) {
                 // Qury balance use balances module
                 throw new Error("Unimplemented");
             } else {
                 // Qury balance use assets module
-                balance = (await api.query.assets.account(token.asset_id, token.account)).toJSON().balance;
+                balance = (await khalaApi.query.assets.account(token.asset_id, token.account)).toJSON().balance;
             }
         } else if (token.chain_bype === 'evm') {
-            const provider = new ethers.providers.JsonRpcProvider(token.endpoint + process.env.ONFINALITY_API_KEY);
+            if (moonriverProvider === 'undefined') {
+                console.log(`Establish connection with evm node`);
+                moonriverProvider = new ethers.providers.JsonRpcProvider(token.endpoint + process.env.ONFINALITY_API_KEY);
+            }
             if (token.is_native) {
                 // Qury balance through network RPC
                 throw new Error("Unimplemented");
             } else {
                 // Qury balance according erc20 protocol
-                const erc20Token = new ethers.Contract(token.contract_address, IERC20Json.abi, provider);
+                const erc20Token = new ethers.Contract(token.contract_address, IERC20Json.abi, moonriverProvider);
                 balance = await erc20Token.balanceOf(token.account);
             }
         } else {
@@ -34,6 +51,6 @@ async function update_balance_of(tokens) {
     }));
 }
 
-module.export = {
+module.exports = {
     update_balance_of,
 }
